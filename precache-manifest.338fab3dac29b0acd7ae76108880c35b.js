4f28b73ac1f8ef06c3c4a662ab7db3d4b7b6b6aa
unknown <tshenvui98@gmail.com>
2019-07-12 12:14:47 +0800

    Updates

---
+++ precache-manifest.338fab3dac29b0acd7ae76108880c35b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1be0319885ab99d169b8c7da7553bf",
+    "url": "/sg_weather/index.html"
+  },
+  {
+    "revision": "e02d8003372df4f6181d",
+    "url": "/sg_weather/static/css/main.4662583f.chunk.css"
+  },
+  {
+    "revision": "8424585924d66086ddc0",
+    "url": "/sg_weather/static/js/2.bcf4d46b.chunk.js"
+  },
+  {
+    "revision": "e02d8003372df4f6181d",
+    "url": "/sg_weather/static/js/main.3a94facb.chunk.js"
+  },
+  {
+    "revision": "635456b2622db7071da3",
+    "url": "/sg_weather/static/js/runtime~main.d0718a26.js"
+  },
+  {
+    "revision": "fbe9bd2867c7b363376b6168911e17c2",
+    "url": "/sg_weather/static/media/cloudy.fbe9bd28.svg"
+  },
+  {
+    "revision": "3ac83e27bda9b31878b7ac95d17ddcf1",
+    "url": "/sg_weather/static/media/rainy.3ac83e27.svg"
+  },
+  {
+    "revision": "029cf241be2d8ed21a3f8e80c6549435",
+    "url": "/sg_weather/static/media/sunny.029cf241.svg"
+  },
+  {
+    "revision": "6ba4f4f8aed6ba305af64050bef86d7d",
+    "url": "/sg_weather/static/media/windy.6ba4f4f8.svg"
+  }
+]);
